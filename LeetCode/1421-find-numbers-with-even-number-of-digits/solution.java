8a2e0af36e8197e64634b0b471b4d0efdb6b7bf0
Sainandhan M <sainandhanmucharla@gmail.com>
2026-09-23 16:54:17 +0000

    Sync LeetCode submission - Find Numbers with Even Number of Digits - Runtime - 1 ms (99.52%), Memory - 44.3 MB (88.84%)

--- LeetCode/1421-find-numbers-with-even-number-of-digits/solution.java
+++ LeetCode/1421-find-numbers-with-even-number-of-digits/solution.java
@@ -2,12 +2,8 @@ class Solution {
     public int findNumbers(int[] nums) {
         int ans = 0;
         for(int n : nums){
-            int cnt = 0;
-            while(n > 0){
-                n = n/10;
-                cnt++;
-            }
-            if(cnt % 2 == 0) ans++;
+            int digits = (int)(Math.log10(n) + 1);
+            if(digits %2 == 0) ans++;
         }
         return ans;
     }
